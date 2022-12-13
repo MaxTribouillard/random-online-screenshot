7c902a0eb3262dfe4bd627483c104095da710936
stardust <57811248+dhrugs@users.noreply.github.com>
2022-12-13 17:23:24 +0100

    Update 1.2
    Now, you can create a .txt file to save links generated.
    Maintenant, vous pouvez créer un fichier .txt afin d'enregistrer les liens générés.

--- random_screenshots.py
+++ random_screenshots.py
@@ -1,19 +1,17 @@
 import random
-from colorama import Style, Fore, Back
+from colorama import Fore
 import string
 import os
 
 # SOON : YOU WILL BE ABLE TO SAVE THE LINK GENERATED !
-# Bientôt : Vous pourrez
-
+# Bientôt : Vous pourrez enregistrer les liens dans un fichier texte
 print(Fore.GREEN + "Bienvenue dans le chercheur de screenshots aléatoire en ligne ! ")
 print(Fore.GREEN + "Welcome in the random screenshots searcher online ! ")
-
-# stock = input(Fore.LIGHTBLUE_EX + "Voulez-vous enregistrer les liens générés dans un fichier texte? (Oui/Non) ")
-# name_Stock = input('Comment voulez vous appeler le fichier de stockage ? ')
 lancement = input(Fore.YELLOW + "Voulez-vous vraiment lancer le générateur de screenshots ? (Oui/Non) : ")
-
-
+stock = input(Fore.LIGHTBLUE_EX + "Voulez-vous enregistrer les liens générés dans un fichier texte? (Oui/Non) ")
+name_Stock = input('Comment voulez vous appeler le fichier de stockage ? ')
+if lancement == str("Non"):
+    exit()
 while lancement == str("Oui"):
     char_list = string.ascii_letters
     second_char_list = string.ascii_letters
@@ -24,5 +22,11 @@ while lancement == str("Oui"):
         Random_Num = random.choice(num_list)
         Second_Random_Num = random.choice(num_list)
         Third_Random_Num = random.choice(num_list)
-        link = Random_Char + Second_Random_Char + Random_Num + Second_Random_Num + Third_Random_Num
-        print(f"https://prnt.sc/{Random_Char}{Second_Random_Char}{Random_Num}{Second_Random_Num}{Third_Random_Num}")
+        link = str("https://prnt.sc/")
+        print(f"{link}{Random_Char}{Second_Random_Char}{Random_Num}{Second_Random_Num}{Third_Random_Num}")
+        # full_Link = str(link) + Random_Char + Second_Random_Char + int(Random_Num) + int(Second_Random_Num) + int(Third_Random_Num)
+        if stock == str("Oui"):
+            file = open(name_Stock+".txt", "w+")
+            file.write(f'Lien : {link+Random_Char+Second_Random_Char+str(Random_Num)+str(Second_Random_Num)+str(Third_Random_Num)}')
+            file.close()
+
